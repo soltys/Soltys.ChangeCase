259c99655dc6fc1645a6fc9c08fbdbcd6e7e1deb
Paweł Sołtysiak <soltys@uznam.org>
2015-04-02 23:03:16 +0200

    changed namespace of changeCase Extensions

--- ChangeCase.cs
+++ ChangeCase.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Soltys
+namespace Soltys.ChangeCase
 {
     public static class ChangeCaseExtensions
     {
